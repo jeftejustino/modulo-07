c68541f605f0ad879ae9ccffc5aade4c87ab64e1
Jefte <jefte.justino@gmail.com>
2019-10-30 22:22:36 -0300

    Desafio 07 Bootcamp Rocketseat

---
+++ src/pages/Home/index.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { MdShoppingCart } from 'react-icons/md';
+import { bindActionCreators } from 'redux';
+import { ProductList } from './styles';
+
+import { formatPrice } from '../../util/format';
+import * as CartActions from '../../store/modules/cart/actions';
+import api from '../../services/api';
+
+class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  handleAddProduct = id => {
+    const { addTocartRequest } = this.props;
+
+    addTocartRequest(id);
+  };
+
+  render() {
+    const { products } = this.state;
+    const { amount } = this.props;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product.id)}
+            >
+              <div>
+                <MdShoppingCart size={16} color="#fff" />{' '}
+                {amount[product.id] || 0}
+              </div>
+
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
